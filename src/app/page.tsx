b8f3c3667ecf84d0c6ab3ef61f27ab5d4f19797d
DannerDQ <diazquispedanner@gmail.com>
2024-11-07 18:41:50 -0500

    Fix Bug 71120240636

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 import styles from "./page.module.css";
 
-export default function Home({ children }: { children: React.ReactNode }) {
+export default function Home() {
   console.log(styles);
 
   return (
     <div>
       <h1>Yacu Selva</h1>
-      {children}
     </div>
   );
 }
