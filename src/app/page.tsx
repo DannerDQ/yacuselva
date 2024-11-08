7f4fa567a5feee3f4d22069cb6143f84207e8f03
DannerDQ <diazquispedanner@gmail.com>
2024-11-07 20:13:17 -0500

    Add Mocked Products && Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,40 @@
+"use client";
+
+import Footer from "@/components/ui/footer";
+import ProductList from "@/components/ui/product-list";
 import styles from "./page.module.css";
 
-export default function Home() {
-  console.log(styles);
+export default function Page() {
+  const products = [
+    {
+      id: 1,
+      title: "Agua de Mesa Yacu Selva",
+      imageUrl: "/garrafa-20l.webp",
+      price: 5,
+      description: "Reposición garrafa de agua 20L",
+    },
+    {
+      id: 2,
+      title: "Agua de Mesa Yacu Selva",
+      imageUrl: "/garrafa-20l.webp",
+      price: 30,
+      description: "Garrafa de agua 20L",
+    },
+  ];
+
+  const handleAddToCart = (productId: number) => {
+    console.log(`Producto agregado al carrito: ID ${productId}`);
+  };
 
   return (
-    <div>
-      <h1>Yacu Selva</h1>
-    </div>
+    <>
+      <main className={styles.main}>
+        <h1>Nuestros Productos:</h1>
+        <br />
+        <ProductList products={products} onAddToCart={handleAddToCart} />
+      </main>
+      <br />
+      <Footer />
+    </>
   );
 }
